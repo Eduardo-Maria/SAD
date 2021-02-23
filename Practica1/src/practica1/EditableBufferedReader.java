68ddbfeaa706c08fcdd27ef17608cacb2679e7d4
Eduardo-Maria <79258653+Eduardo-Maria@users.noreply.github.com>
2021-02-23 11:00:52 +0100

    Add files via upload

---
+++ Practica1/src/practica1/EditableBufferedReader.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+import java.io.*;
+
+
+public class EditableBufferedReader extends BufferedReader{
+
+    public EditableBufferReader() {
+    }
+    
+    
+    setRaw: passa la consola de mode cooked a mode raw.
+unsetRaw: passa la consola de mode raw a mode cooked.
+read: llegeix el següent caràcter o la següent tecla de cursor.
+readLine: 
+    
+}
