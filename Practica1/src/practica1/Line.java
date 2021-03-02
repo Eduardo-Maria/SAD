2f2368f3749d8fec567d575be8be041f3084a8c9
Eduardo-Maria <79258653+Eduardo-Maria@users.noreply.github.com>
2021-03-02 10:25:29 +0100

    Add files via upload

---
+++ Practica1/src/practica1/Line.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+/**
+ *
+ * @author maria
+ */
+public class Line {
+
+    public Line() {
+    }
+    
+    public void addChar(char c) {
+
+    }
+
+    public void left() {
+        
+    }
+    
+    public void right() {
+        
+    }
+    
+    public void up() {
+        
+    }
+    
+    public void down() {
+        
+    }
+
+    public void delete() {
+        
+    }
+    
+    public void insert() {
+        
+    }
+
+    public void home() {
+        
+    }
+
+    public void end() {
+        
+    }
+    
+    @Override
+    public String toString() {
+        return "Line{" + '}';
+    }
+}
